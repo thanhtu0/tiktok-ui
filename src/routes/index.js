eed22856e1d4eb87b8d3c220f8b51ce6456939e9
thanhtu0 <83869938+thanhtu0@users.noreply.github.com>
2024-07-29 15:03:17 +0700

    Builder header Logic part-2

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 // Layouts
 import { HeaderOnly } from '~/components/Layout';
 
+// Pages
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
@@ -11,7 +12,7 @@ import Search from '~/pages/Search';
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
-    { path: '/profile', component: Profile },
+    { path: '/@:nickname', component: Profile },
     { path: '/upload', component: Upload, layout: HeaderOnly },
     { path: '/search', component: Search, layout: null },
 ];
