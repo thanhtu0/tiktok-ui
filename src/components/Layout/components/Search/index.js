d103a509bbfaf75d51250781844af1b3ab21c1ea
thanhtu0 <83869938+thanhtu0@users.noreply.github.com>
2024-07-29 16:18:10 +0700

    Learned and integrated Axios for API requests
    - Studied Axios documentation and usage
    - Installed and configured Axios in the project
    - Replaced fetch requests with Axios for better error handling and code structure

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -3,6 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import HeadlessTippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
+
+import * as searchServices from '~/apiServices/searchServices';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icons';
@@ -17,10 +19,6 @@ function Search() {
     const [showResult, setShowResult] = useState(true);
     const [loading, setLoading] = useState(false);
 
-    // 1: ''
-    // 2: 'h'
-    // 3: 'ho'
-    // 4: 'hoa'
     const debounced = useDebounce(searchValue, 500);
 
     const inputRef = useRef();
@@ -31,17 +29,16 @@ function Search() {
             return;
         }
 
-        setLoading(true);
+        const fetchApi = async () => {
+            setLoading(true);
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
-            .then((res) => res.json())
-            .then((res) => {
-                setSearchResult(res.data);
-                setLoading(false);
-            })
-            .catch(() => {
-                setLoading(false);
-            });
+            const result = await searchServices.search(debounced);
+
+            setSearchResult(result);
+            setLoading(false);
+        };
+
+        fetchApi();
     }, [debounced]);
 
     const handleClear = () => {
