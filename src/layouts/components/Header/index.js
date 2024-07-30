5847db7753e7e5b4a5dde79ea02ec1040ffa6964
thanhtu0 <83869938+thanhtu0@users.noreply.github.com>
2024-07-30 09:00:10 +0700

    Refactor: reorganize project structure

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -13,7 +13,7 @@ import {
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 import { Link } from 'react-router-dom';
-import routesConfig from '~/config/routes';
+import config from '~/config';
 
 import Button from '~/components/Button';
 import styles from './Header.module.scss';
@@ -217,7 +217,7 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <Link to={routesConfig.home} className={cx('logo-link')}>
+                <Link to={config.routes.home} className={cx('logo-link')}>
                     <img src={images.logo} alt="Tiktok" />
                 </Link>
 
